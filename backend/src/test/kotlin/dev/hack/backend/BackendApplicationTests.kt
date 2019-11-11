6ee763bb374452c5776f84f27eadfed60d58d188
Mazunin Konstantin <mazuninky@gmail.com>
2019-11-11 18:16:25 +0300

    Add template for backend

---
+++ backend/src/test/kotlin/dev/hack/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.hack.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
